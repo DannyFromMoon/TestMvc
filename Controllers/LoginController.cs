c4b8aa417db3a2e4293b9d4b3df3d0ad2b8c680c
wangyong <1135137305@qq.com>
2019-03-01 16:27:16 +0800

    Initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMvc.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CheckUser(string name, string address)
+        {
+
+
+
+
+            return Json("{'name':1}", JsonRequestBehavior.AllowGet);
+
+            //return Content("{'name':1}");
+        }
+    }
+}
